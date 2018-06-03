215a4afd1f031fda0be010244d0af243c7e6118a
Rizwan <djrizwanmixing@gmail.com>
2018-06-03 13:51:53 +0530

    Upgraded to Laravel 5.3 from 5.2

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -20,7 +20,7 @@ class AdminPostsController extends Controller
      */
     public function index()
     {
-        $posts = Post::paginate(25);
+        $posts = Post::paginate(2);
 
         return view('admin.posts.index', compact('posts'));
     }
@@ -32,7 +32,7 @@ class AdminPostsController extends Controller
      */
     public function create()
     {
-        $categories = Category::lists('name','id')->all();
+        $categories = Category::pluck('name','id')->all();
         return view('admin.posts.create', compact('categories'));
         
     }
@@ -101,7 +101,7 @@ class AdminPostsController extends Controller
     {
         $post = Post::findorFail($id);
         
-        $categories = Category::lists('name', 'id')->all();
+        $categories = Category::pluck('name', 'id')->all();
         
         return view('admin.posts.edit', compact('post','categories'));
     }
